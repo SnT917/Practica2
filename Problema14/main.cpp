f87182942988086b5ab34d294c0e185b6fe2332b
SnT917 <santiago.narvaezt@udea.edu.co>
2018-04-13 08:35:27 -0500

    Problema #14: Rotación de matriz.

--- Problema14/main.cpp
+++ Problema14/main.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
-#include <conio.h>
-#include <stdlib.h>
 #include <time.h>
+
 using namespace std;
-
-//Problema 14. Elabore un programa que llene una matriz 5x5 con los números del 1 al 25 y la imprima, luego
-//imprima la matriz rotada 90, 180 y 270 grados.
-//Ejemplo:
-
-//Matriz Original                   Matriz Rotada 90°
-//1  2  3  4  5                       21 16 11 6  1
-//6  7  8  9  10                      22 17 12 7  2
-//11 12 13 14 15                      23 18 13 8  3
-//16 17 18 19 20                      24 19 14 9  4
-//21 22 23 24 25                      25 20 15 10 5
+void printM (int, int (*mat)[5]);
+void rotateM(int (*M)[5], int (*m)[5], int);
 
 int main()
 {
-    int fil=5, col=5; //Generamos la matriz aleatoriamente de orden 5x5.
-    int matriz[fil][col];
-    srand(time(NULL));
-    for(int filas=0; filas<fil; filas++){
-        for(int columnas=0; columnas<col; columnas++){
-            matriz[filas][columnas] = rand()% 25 + 1; //Damos valores a las filas y columnas desde 1 hasta 25.
+   int n=5;
+   int mat1[5][5]={0}, mat2[5][5]={0};
+   srand(time(NULL));
+   for(int filas=0; filas<n; filas++){
+       for(int columnas=0; columnas<n; columnas++){
+           mat1[filas][columnas] = rand()% 25 + 1; //Damos valores a las filas y columnas desde 1 hasta 25.
+           }
+       }
+   cout<<"Matriz original: "<<endl;
+   printM(n,mat1);
+   rotateM(mat1,mat2,n);
+   cout<<endl<<"Matriz rotada 90 grados "<<endl;
+   printM(n,mat2);
+   rotateM(mat2,mat1,n);
+   cout<<endl<<"Matriz rotada 180 grados "<<endl;
+   printM(n,mat1);
+   rotateM(mat1,mat2,n);
+   cout<<endl<<"Matriz rotada 270 grados "<<endl;
+   printM(n,mat2);
+    return 0;
+}
+
+void printM (int n, int (*mat)[5]){
+    for(int i=0;i<n; i++){
+        for(int j=0; j<n; j++){
+            if(n*n>=100){
+                if(*(*(mat+i)+j)>=100)cout<<*(*(mat+i)+j)<<" ";
+                else if(*(*(mat+i)+j)>=10)cout<<" "<<*(*(mat+i)+j)<<" ";
+                else cout<<" "<< *(*(mat+i)+j)<<" ";
+            }
+            else{
+                if(*(*(mat+i)+j)>=10)cout <<*(*(mat+i)+j)<<" ";
+                else cout<<" "<<*(*(mat+i)+j)<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void rotateM(int (*M)[5], int (*m)[5], int n){
+    for(int i=0, l=0; i<n; i++, l++){
+        for(int j=0, k=n-1; j<n; j++, k--){
+            *(*(m+i)+j) = *(*(M+k)+l);
         }
     }
-
-    cout<<"Matriz Original:"<<endl;
-    for(int filas=0; filas<fil; filas++){
-        for(int columnas=0; columnas<col; columnas++){ //Imprimimos la matriz original.
-            cout << matriz[columnas][filas]<<"  ";
-        }cout<<endl;
-    }
-
-    cout<<"Matriz Rotada 90:"<<endl;
-    for(int filas=0; filas<fil; filas++){
-        for(int columnas=0; columnas<col; columnas++){ //Imprimimos la matriz rotada en 90 grados.
-            cout << matriz[filas][columnas]<<"  ";
-        }cout<<endl;
-    }
-    return 0;
 }
